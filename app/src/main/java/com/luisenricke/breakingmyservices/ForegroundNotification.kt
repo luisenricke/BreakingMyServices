27e9b8ff3d1ebab03eafce0c78470fec41e6b3b5
Luis Villalobos <luisevm24@outlook.com>
2020-07-11 19:25:32 -0500

    format code

--- app/src/main/java/com/luisenricke/breakingmyservices/ForegroundNotification.kt
+++ app/src/main/java/com/luisenricke/breakingmyservices/ForegroundNotification.kt
@@ -2,11 +2,11 @@ package com.luisenricke.breakingmyservices
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_DEFAULT
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
@@ -14,11 +14,18 @@ import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 
+// reference: https://androidwave.com/foreground-service-android-example-in-kotlin/
 class ForegroundNotification : Service() {
 
-    private val CHANNEL_ID = "ForegroundService Kotlin"
-
     companion object {
+        private const val FOREGROUND_ID = 1010
+
+        private const val CHANNEL_ID = "ForegroundNotificationChannelID"
+        private const val CHANNEL_NAME = "Foreground Notification Channel"
+
+        private const val NOTIFICATION_TITLE = "Foreground Service Kotlin Example"
+        private const val NOTIFICATION_ICON = android.R.drawable.ic_dialog_alert
+
         fun startService(context: Context, message: String) {
             val startIntent = Intent(context, ForegroundNotification::class.java)
             startIntent.putExtra("inputExtra", message)
@@ -39,8 +46,6 @@ class ForegroundNotification : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         // return super.onStartCommand(intent, flags, startId)
-        val icon = BitmapFactory.decodeResource(resources, android.R.drawable.ic_menu_upload)
-
         Log.i("ForegroundNotification", "Sending notification")
 
         val input = intent?.getStringExtra("inputExtra")
@@ -48,30 +53,24 @@ class ForegroundNotification : Service() {
         val notificationIntent = Intent(this, MainActivity::class.java)
         val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
         val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("Foreground Service Kotlin Example")
+            .setContentTitle(NOTIFICATION_TITLE)
             .setContentText(input)
-            .setSmallIcon(android.R.drawable.ic_notification_clear_all)
+            .setSmallIcon(NOTIFICATION_ICON)
             .setContentIntent(pendingIntent)
             .build()
 
-        startForeground(1, notification)
-        //stopSelf();
+        startForeground(FOREGROUND_ID, notification)
 
         return START_STICKY
     }
 
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val serviceChannel = NotificationChannel(
-                CHANNEL_ID,
-                "Foreground Service Channel",
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-            val manager = getSystemService(NotificationManager::class.java)
-            manager!!.createNotificationChannel(serviceChannel)
+            val serviceChannel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, IMPORTANCE_DEFAULT)
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(serviceChannel)
         }
     }
 
     override fun onBind(intent: Intent?): IBinder? = null // doesn't bind any view in this services
-
 }
